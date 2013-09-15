c5888a489d520b08e4177449f99e813477e2dd6d
halilayyildiz <halil.ayyildiz@gmail.com>
2013-09-15 20:44:36 +0300

    content length fix

--- fablebox-server/fablebox/app/controllers/api/fable_controller.rb
+++ fablebox-server/fablebox/app/controllers/api/fable_controller.rb
@@ -10,16 +10,19 @@ class Api::FableController < ApplicationController
   def audio
     file_path = Api::PATH_FABLE_AUDIO + params[:guid]
     send_file file_path
+    headers['Content-Length'] = File.size(file_path).to_s
   end
 
   def image_small
     file_path = Api::PATH_FABLE_IMAGE_SMALL + params[:guid]
     send_file file_path
+    headers['Content-Length'] = File.size(file_path).to_s
   end
 
   def image_large
     file_path = Api::PATH_FABLE_IMAGE_LARGE + params[:guid]
     send_file file_path
+    headers['Content-Length'] = File.size(file_path).to_s
   end
 
 
